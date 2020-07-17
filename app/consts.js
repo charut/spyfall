62178b5782663782a98c9f4664d7176cbae2ca00
Emre <charut@gmail.com>
2020-07-17 16:08:09 -0400

    Update consts.js
    add 3 players

--- app/consts.js
+++ app/consts.js
@@ -1,5 +1,5 @@
 export const MIN_PLAYERS = 3;
-export const MAX_PLAYERS = 12;
+export const MAX_PLAYERS = 15;
 export const MAX_ROLES = MAX_PLAYERS - 2;
 export const MAX_ROLES_ARRAY = new Array(MAX_ROLES).fill('');
 export const SPY_ROLE = 'spy';
